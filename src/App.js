d0c6a1b7a05b3d8ecf8994b27a5303ac440be41b
Long <marcuskuuhaku@gmail.com>
2020-04-05 18:48:11 +0700

    Revert "Add changed files"
    This reverts commit d2b855a04087aa9567fc9df44b4199c5dd52c13a.

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./styles.css";
+import ShoppingTable from "./components/ShoppingTable/ShoppingTable.js";
+
+export default function App() {
+  return (
+      <ShoppingTable>
+      </ShoppingTable>
+  );
+}
