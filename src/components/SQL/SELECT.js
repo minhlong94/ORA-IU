f67cf87c7f1c4ac037d42e3d2a3a1d0db04e2b68
Long <marcuskuuhaku@gmail.com>
2020-04-08 20:21:24 +0700

    Completed paignation
    - Shopping Table now has paignation
    - MySQL js file to export data into json file

---
+++ src/components/SQL/SELECT.js
@@ -0,0 +1,32 @@
+var mysql = require('mysql');
+var fs = require('fs');
+
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "MinhLong94",
+    database: "pdm"
+});
+
+// Export database into table.json file
+con.connect();
+con.query('select * from pdm.newonlineretail order by ItemDes', function(err, results, fields) {
+    if(err) throw err;
+
+    fs.writeFile('table.json', JSON.stringify(results), function (err) {
+        if (err) throw err;
+        console.log('Saved!');
+    });
+    con.end();
+});
+
+
+
+
+
+
+
+
+
+
+
--- src/components/ShoppingTable/ShoppingTable.js
+++ src/components/ShoppingTable/ShoppingTable.js
@@ -1,4 +1,5 @@
 import React from 'react';
+<<<<<<< Updated upstream
 
 export default class ShoppingTable extends React.Component {
     render() {
@@ -64,4 +65,170 @@ export default class ShoppingTable extends React.Component {
         </table>
         )
     }
+=======
+import table from "../SQL/table";
+import { useTable, usePagination } from 'react-table';
+
+/*Forked from https://github.com/tannerlinsley/react-table/tree/master/examples/pagination */
+
+function Table({ columns, data }) {
+    // Use the state and functions returned from useTable to build your UI
+    const {
+        getTableProps,
+        getTableBodyProps,
+        headerGroups,
+        prepareRow,
+        page, // Instead of using 'rows', we'll use page,
+        // which has only the rows for the active page
+
+        // The rest of these things are super handy, too ;)
+        canPreviousPage,
+        canNextPage,
+        pageOptions,
+        pageCount,
+        gotoPage,
+        nextPage,
+        previousPage,
+        setPageSize,
+        state: { pageIndex, pageSize },
+    } = useTable(
+        {
+            columns,
+            data,
+            initialState: { pageIndex: 0 },
+        },
+        usePagination
+    )
+
+    // Render the UI for your table
+    return (
+        <>
+      {/*<pre>*/}
+      {/*  <code>*/}
+      {/*    {JSON.stringify(*/}
+      {/*        {*/}
+      {/*            pageIndex,*/}
+      {/*            pageSize,*/}
+      {/*            pageCount,*/}
+      {/*            canNextPage,*/}
+      {/*            canPreviousPage,*/}
+      {/*        },*/}
+      {/*        null,*/}
+      {/*        2*/}
+      {/*    )}*/}
+      {/*  </code>*/}
+      {/*</pre>*/}
+            <table class="blueTable" {...getTableProps()}>
+                <thead>
+                {headerGroups.map(headerGroup => (
+                    <tr {...headerGroup.getHeaderGroupProps()}>
+                        {headerGroup.headers.map(column => (
+                            <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+                        ))}
+                    </tr>
+                ))}
+                </thead>
+                <tbody {...getTableBodyProps()}>
+                {page.map((row, i) => {
+                    prepareRow(row)
+                    return (
+                        <tr {...row.getRowProps()}>
+                            {row.cells.map(cell => {
+                                return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+                            })}
+                        </tr>
+                    )
+                })}
+                </tbody>
+            </table>
+            {/*
+        Pagination can be built however you'd like.
+        This is just a very basic UI implementation:
+      */}
+            <div className="pagination">
+                <button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
+                    {'<<'}
+                </button>{' '}
+                <button onClick={() => previousPage()} disabled={!canPreviousPage}>
+                    {'<'}
+                </button>{' '}
+                <button onClick={() => nextPage()} disabled={!canNextPage}>
+                    {'>'}
+                </button>{' '}
+                <button onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage}>
+                    {'>>'}
+                </button>{' '}
+                <span>
+          Page{' '}
+                    <strong>
+            {pageIndex + 1} of {pageOptions.length}
+          </strong>{' '}
+        </span>
+                <span>
+          | Go to page:{' '}
+                    <input
+                        type="number"
+                        defaultValue={pageIndex + 1}
+                        onChange={e => {
+                            const page = e.target.value ? Number(e.target.value) - 1 : 0
+                            gotoPage(page)
+                        }}
+                        style={{ width: '100px' }}
+                    />
+        </span>{' '}
+                <select
+                    value={pageSize}
+                    onChange={e => {
+                        setPageSize(Number(e.target.value))
+                    }}
+                >
+                    {[10, 20, 30, 40, 50].map(pageSize => (
+                        <option key={pageSize} value={pageSize}>
+                            Show {pageSize}
+                        </option>
+                    ))}
+                </select>
+            </div>
+        </>
+    )
+}
+
+export default function ShoppingTable() {
+    const columns = React.useMemo(
+        () => [
+            {
+                Header: "Shopping Table",
+                columns: [
+                    {
+                        Header: 'Stock Code',
+                        accessor: 'StockCode',
+                    },
+                    {
+                        Header: 'Item Name',
+                        accessor: 'ItemDes',
+                    },
+                    {
+                        Header: 'Unit Price',
+                        accessor: 'UnitPrice',
+                    },
+                    {
+                        Header: 'In Stock',
+                        accessor: 'QUANTITY',
+                    },
+                    {
+                        Header: 'Add to cart',
+                        accessor: null,
+                    }
+                ],
+            }
+        ],
+        []
+    )
+
+    const data = table;
+
+    return (
+            <Table columns={columns} data={data} />
+    )
+>>>>>>> Stashed changes
 }
