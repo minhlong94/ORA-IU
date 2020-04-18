0a35bb66356583ba364da980c54ddac6e9f16d73
Doki <minhdiep1280@gmail.com>
2020-04-18 20:21:33 +0700

    Update Navigation Bar.

--- src/components/Utils/NavigationBar.js
+++ src/components/Utils/NavigationBar.js
@@ -4,17 +4,22 @@ import React from "react";
 export default function NavigationBar() {
     return (
         <Navbar bg="dark" variant="dark">
-            <Navbar.Brand href="/">Navbar</Navbar.Brand>
+            <Navbar.Brand href="/">
+                <img src="https://react-bootstrap.github.io/logo.svg"
+                     width="30"
+                     height="30"
+                     className="d-inline-block align-top"
+                     alt="React Bootstrap logo"/>
+            </Navbar.Brand>
             <Nav className="mr-auto">
-                <Nav.Link href="/">Home</Nav.Link>
+                <Nav.Link href="/items">Items</Nav.Link>
+                <Nav.Link href="/cart">Cart</Nav.Link>
+            </Nav>
+
+            <Nav>
                 <Nav.Link href="/login">Log In</Nav.Link>
                 <Nav.Link href="/signup">Sign Up</Nav.Link>
-                <Nav.Link href="/items">Items</Nav.Link>
             </Nav>
-            <Form inline>
-                <Form.Control type="text" placeholder="Search" className="mr-sm-2" />
-                <Button variant="info">Search</Button>
-            </Form>
         </Navbar>
     )
 }
